42aa62d62b29eed1c08fa450ad4ffe6441fd82fe
Pallavi <pallavishettigar03@gmail.com>
2024-01-21 11:50:22 +0530

    add changes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const quoteText = document.querySelector (".quote"),
+authorname = document.querySelector(".author .name")
 quoteBtn = document.querySelector("button");
 
 function randomQuote(){
-    console.log("clciked")
+    fetch("http://api.quotable.io/random").then(res=> res.json()).then (result =>{
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+    })
 }
 quoteBtn.addEventListener("click", randomQuote);
